c8dbe841a3c461076eb17520a3c1eb90daded4df
Sahsa <a.v.pavlov09@gmail.com>
2022-10-15 20:44:43 +0300

    Метод генерации символьной строки + метод вывода на печать

--- VW/Program.cs
+++ VW/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+string FillText(string text, int wordCount)
+{
+    string librarySymbol = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";//библиотека символов
+
+    for (int i = 0; i < wordCount; i++)
+    {
+        string word = String.Empty;
+        int wordLength = new Random().Next(1, 15); //рандомна длина слова
+        for (int j = 0; j < wordLength; j++)
+        {
+            word = word + librarySymbol[new Random().Next(0, 62)]; //сбор слова из символов
+        }
+        text += word + " ";
+    }
+    return text;
+}
+string PrintText(string text)
+{
+    return "|" + String.Join(' ', text) + "|";
+}
+
+int wordCount = new Random().Next(2, 30);//рандомное количество слов
+
+string text = String.Empty;
+text = FillText(text, wordCount);
+Console.WriteLine(PrintText(text));
